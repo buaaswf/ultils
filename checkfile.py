e469e62c013012874c13fb484a3d7a27151267c3
sdf0615 <876267233@qq.com>
2019-02-21 10:45:34 +0800

    check files need

---
+++ checkfile.py
@@ -0,0 +1,7 @@
+###input two directories with same subdirectories structures:
+#### A with subdirectories and B with subdirectories
+#### select the same subdirectories and iamge files in its subdirectories from A with B.
+#### makedir C to copy the files from A
+def checkfiles(Apath, Bpath):
+  return samefiles
+  
